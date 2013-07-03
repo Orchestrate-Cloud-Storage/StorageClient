d5c2ad3476cb7e51f0d697ae805f0ac523f77c4e
Sean Cassidy <scassidy@llnw.com>
2013-07-03 13:42:23 -0700

    Limit depth to ten

--- src/main/java/com/llnw/storage/client/EndpointHTTP.java
+++ src/main/java/com/llnw/storage/client/EndpointHTTP.java
@@ -476,7 +476,9 @@ public class EndpointHTTP implements EndpointMultipart {
         // exception we want to throw when interrupted, ClosedByInterruptException. So, try to find that
         // exception in the stack, and then throw that one instead
         Throwable t = e.getCause();
-        while (t != null && !(t instanceof ClosedByInterruptException)) {
+
+        int i = 10; // cause depth limit
+        while (i-- > 0 && t != null && !(t instanceof ClosedByInterruptException)) {
             t = t.getCause();
         }
 
