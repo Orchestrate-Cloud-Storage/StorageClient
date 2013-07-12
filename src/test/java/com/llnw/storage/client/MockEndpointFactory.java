2474400842ee520901850fa797d9a9ff7be655e4
Brandon Smith <bsmith@llnw.com>
2013-07-11 17:51:54 -0700

    Allow for destination file in mock endpoint

--- src/test/java/com/llnw/storage/client/MockEndpointFactory.java
+++ src/test/java/com/llnw/storage/client/MockEndpointFactory.java
@@ -1,6 +1,7 @@
 package com.llnw.storage.client;
 
 import com.google.common.collect.Lists;
+import com.google.common.io.Files;
 import com.llnw.storage.client.io.ActivityCallback;
 
 import javax.annotation.Nullable;
@@ -10,9 +11,18 @@ import java.io.IOException;
 import java.util.List;
 
 public class MockEndpointFactory extends EndpointFactory {
+
+    private final File destination;
+
     public MockEndpointFactory() {
-        super(null, null, null);
+        this(null);
     }
+
+    public MockEndpointFactory(File destination) {
+        super(null, null, null);
+        this.destination = destination;
+    }
+
 
     @Override
     public Endpoint create(boolean useFTP) {
@@ -41,6 +51,9 @@ public class MockEndpointFactory extends EndpointFactory {
             @Override
             public void upload(File file, String path, String name, @Nullable ActivityCallback callback)
                     throws IOException {
+                if (destination != null) {
+                    Files.copy(file, destination);
+                }
             }
 
             @Override
